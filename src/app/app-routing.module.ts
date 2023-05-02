9a794c4bdc0c74547bb3a4712e22d0d4acba5a51
GabrielChuchuca <gavriellalx@hotmail.com>
2023-05-02 17:37:01 -0500

    Actualizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MapaComponent } from './components/mapa/mapa.component';
 import { AppComponent } from './app.component';
 import { Mapa2Component } from './components/mapa2/mapa2.component';
+import { GraficaComponent } from './components/grafica/grafica.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/mapa', pathMatch: 'full' },
   { path: 'mapa', component: MapaComponent },
   { path: 'mapa2', component: Mapa2Component},
+  { path: 'grafica', component: GraficaComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MapaComponent } from './components/mapa/mapa.component';
 import { Mapa2Component } from './components/mapa2/mapa2.component';
+import { GraficaComponent } from './components/grafica/grafica.component';
+
+import * as CanvasJSAngularChart from '../assets/canvasjs-3.7.5/canvasjs.angular.component';
+let CanvasJSChart = CanvasJSAngularChart.CanvasJSChart;
 
 @NgModule({
   declarations: [
     AppComponent,
     MapaComponent,
     Mapa2Component,
+    GraficaComponent,
+    CanvasJSChart
   ],
   imports: [
     BrowserModule,
